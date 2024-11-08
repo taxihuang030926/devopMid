1d1f2ad3fb69f92f12ee485a64df5e6ac661783e
Tim Huang <taxihuang030926@gmail.com>
2024-11-08 15:21:55 +0800

    login init commit

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
