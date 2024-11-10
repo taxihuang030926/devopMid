697f818b2898102b45df87ff0f69e8ab48f88f63
kunggggggg <anniekung101131@gmail.com>
2024-11-10 19:24:00 +0800

    db update

--- db/student.sql
+++ db/student.sql
@@ -4,13 +4,14 @@ create table Student(
     Ttl_Credit INT,
     S_pwd VARCHAR(20),
     dept VARCHAR(20),
+    Grade INT,
     Class VARCHAR(8),
     foreign key(dept) references Department(Dept_Name)
 );
 
 insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1149888", "Std_A", 0, "888", "資訊", 3, 2);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1152274", "Std_B", 0, "274", "資訊", 3, 3);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1149782", "Std_C", 0, "782", "資訊", 2, 2);
-insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1150267", "Std_D", 0, "267", "資訊", 1, 3);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1152274", "Std_B", 0, "274", "資訊", 3, 2);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1149782", "Std_C", 0, "782", "資訊", 3, 2);
+insert into Student(S_ID, Name, Ttl_Credit, S_pwd, dept, Grade, Class) values("D1150267", "Std_D", 0, "267", "資訊", 3, 2);
 
 select * from Student;
