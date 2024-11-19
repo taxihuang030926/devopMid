d92bdf2c39ad6e7fb9a56bdd7c6ea57edb909018
kunggggggg <anniekung101131@gmail.com>
2024-11-20 02:10:19 +0800

    course and course session update

--- db/Course_Session.sql
+++ db/Course_Session.sql
@@ -20,19 +20,43 @@ insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Se
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1313-1", 1313, "Monday", 8, 8, "土水304");
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1313-2", 1313, "Monday", 9, 9, "土水304");
 
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1318-1", 1318, "Tuesday", 5, 19, "資電403");
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1318-2", 1318, "Tuesday", 6, 20, "資電403");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1318-1", 1318, "Tuesday", 6, 20, "資電403");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1318-2", 1318, "Tuesday", 7, 21, "資電403");
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1318-3", 1318, "Friday", 4, 60, "資電403");
 
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1323-1", 1323, "Wednesday", 6, 34, "資電234");
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1323-2", 1323, "Wednesday", 7, 35, "資電234");
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1323-3", 1323, "Wednesday", 8, 36, "資電234");
 
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1439-1", 1439, "Monday", 1, 1, "資電415");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1439-2", 1439, "Monday", 2, 2, "資電415");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1440-1", 1440, "Tuesday", 8, 22, "資電415");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1440-2", 1440, "Tuesday", 9, 23, "資電415");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1323-1", 1323, "Wednesday", 2, 30, "資電402");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("1323-2", 1323, "Wednesday", 3, 31, "資電402");
+
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2911-1", 2911, "Friday", 6, 62, "語文202");
 insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2911-2", 2911, "Friday", 7, 63, "語文202");
 
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3026-1", 3026, "Friday", 4, 60, "語文202");
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3026-2", 3026, "Friday", 5, 61, "語文202");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3026-1", 3026, "Friday", 3, 59, "語文202");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3026-2", 3026, "Friday", 4, 60, "語文202");
 
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2984-1", 2984, "Wednesday", 6, 34, "人言507");
-insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2984-2", 2984, "Wednesday", 7, 35, "人言507");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3055-1", 3055, "Thursday", 3, 45, "人言507");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3055-2", 3055, "Thursday", 4, 46, "人言507");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2984-1", 2984, "Monday", 11, 11, "商學204");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("2984-2", 2984, "Monday", 12, 12, "商學204");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3077-1", 3077, "Friday", 1, 57, "商學205");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3077-2", 3077, "Friday", 2, 58, "商學205");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3079-1", 3079, "Monday", 8, 64, "商學304");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3079-2", 3079, "Monday", 9, 65, "商學304");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3083-1", 2984, "Thursday", 6, 47, "商學207");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3083-2", 2984, "Thursday", 7, 48, "商學207");
+
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3084-1", 3084, "Thursday", 8, 11, "商學208");
+insert into Course_Session(Session_ID, Course_ID, Session_Day, Session_RTime, Session_Time, Classroom) values("3084-2", 3084, "Thursday", 9, 12, "商學208");
--- db/Courses.sql
+++ db/Courses.sql
@@ -13,7 +13,15 @@ insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Cou
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1313, "微處理器系統實習", "資訊", 1, "三乙", "郭崇韋", 1);
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1318, "系統程式", "資訊", 1, "三乙", "劉宗杰", 3);
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1323, "軟體工程開發實務", "資訊", 0, "三乙", "許懷中", 3);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1439, "編譯器", "資訊", 0, "三乙", "陳青文", 3);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1440, "資訊與網路安全", "資訊", 0, "三乙", "張舜賢", 3);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(1323, "寬頻網路", "資訊", 0, "三乙", "竇其仁", 3);
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(2911, "德文（一）", "外語", 0, NULL, "游曉嵐", 2);
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3026, "德文（一）", "外語", 0, NULL, "王允闐", 2);
 insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(2984, "音樂行旅 - 巡訪古典音樂大師", "通識", 0, NULL, "曾韻心", 2);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3055, "台灣環境與永續發展", "通識", 0, NULL, "鄭翰", 2);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3077, "環境教育", "通識", 0, NULL, "葉怡巖", 2);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3079, "人生哲學", "通識", 0, NULL, "經觀榮", 2);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3083, "中國文物欣賞", "通識", 0, NULL, "戴瑞坤", 2);
+insert into Courses(Course_ID, Course_Name, dept, prereq, Class, Instructor, Course_Credit) values(3084, "佛家思想", "通識", 0, NULL, "鄭保村", 2);
 select * from Courses;
